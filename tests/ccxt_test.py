9f00cfe2c23a4402fec54f4e510974156c87790e
1eehanjin <jdhanjin@naver.com>
2023-12-05 23:39:27 +0900

    바이낸스 loan borrow 코드 작성, 테스트는 ip 제한으로 집에서 돌려보기

--- tests/ccxt_test.py
+++ tests/ccxt_test.py
@@ -3,7 +3,7 @@ import ccxt
 import time
 import json
 
-def generate_timestamp(self):
+def generate_timestamp():
     # 타임스탬프 생성
     timestamp = int(time.time() * 1000)
     return timestamp
@@ -28,18 +28,28 @@ class CcxtTests(unittest.TestCase):
         print(dir(ccxt.binance())) 
 
     def test_get_flexible_loan_data(self):
-        timestamp = generate_timestamp
+        timestamp = generate_timestamp()
         params_loanable_assets = {
             'loanCoin': 'ETH',
             'timestamp': timestamp
         }
-        print(self.binance_with_key.sapiGetLoanFlexibleLoanableData())
+        print(self.binance_with_key.sapiGetLoanFlexibleLoanableData(params=params_loanable_assets))
+
+    def test_loan_borrow(self):
+        timestamp = generate_timestamp()
+        params_loan_borrow = {
+        'loanCoin' : 'BNT',
+        'collateralCoin': 'USDT',
+        'collateralAmount' : 500,
+        'timestamp': timestamp,
+    }
+        print(self.binance_with_key.sapiPostLoanFlexibleBorrow(params=params_loan_borrow))
 
 # unittest를 실행
 if __name__ == '__main__':  
     # 테스트 스위트 생성
     suite = unittest.TestSuite()
-    suite.addTest(CcxtTests('test_retrieve_flexible_loan_data'))  # 실행할 테스트 추가
+    suite.addTest(CcxtTests('test_loan_borrow'))  # 실행할 테스트 추가
     # suite.addTest(MyTestCase('test_two'))  # 다른 테스트 추가
 
     # 테스트 실행
