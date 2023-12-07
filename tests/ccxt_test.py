42d2e96da2ccde3b5dfc70cc992023f2bc200bf0
1eehanjin <12181670@office.inha.ac.kr>
2023-12-08 01:30:31 +0900

    loan borrow, cross margin borrow 테스트 완료

--- tests/ccxt_test.py
+++ tests/ccxt_test.py
@@ -27,7 +27,7 @@ class CcxtTests(unittest.TestCase):
     def test_print_available_binance_functions(self):
         print(dir(ccxt.binance())) 
 
-    def test_get_flexible_loan_data(self):
+    def test_binance_get_flexible_loan_data(self):
         timestamp = generate_timestamp()
         params_loanable_assets = {
             'loanCoin': 'ETH',
@@ -35,21 +35,30 @@ class CcxtTests(unittest.TestCase):
         }
         print(self.binance_with_key.sapiGetLoanFlexibleLoanableData(params=params_loanable_assets))
 
-    def test_loan_borrow(self):
+    def test_binance_loan_borrow(self):
         timestamp = generate_timestamp()
         params_loan_borrow = {
-        'loanCoin' : 'BNT',
+        'loanCoin' : 'AAVE',
         'collateralCoin': 'USDT',
-        'collateralAmount' : 500,
+        'collateralAmount' : 500, #USDT 기준이다.
         'timestamp': timestamp,
-    }
+        }
         print(self.binance_with_key.sapiPostLoanFlexibleBorrow(params=params_loan_borrow))
+
+    def test_binance_cross_margin_borrow(self): 
+        timestamp = generate_timestamp()
+        params_margin_loan = {
+        'asset' : 'AAVE',
+        'amount': 1, #USDT 기준이 아닌 빌리는 코인 기준으로 수량 입력해야 한다.
+        'timestamp': timestamp,
+        }
+        print(self.binance_with_key.sapiPostMarginLoan(params=params_margin_loan))
 
 # unittest를 실행
 if __name__ == '__main__':  
     # 테스트 스위트 생성
     suite = unittest.TestSuite()
-    suite.addTest(CcxtTests('test_loan_borrow'))  # 실행할 테스트 추가
+    suite.addTest(CcxtTests('test_binance_cross_margin_borrow'))  # 실행할 테스트 추가
     # suite.addTest(MyTestCase('test_two'))  # 다른 테스트 추가
 
     # 테스트 실행
