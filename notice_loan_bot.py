400b17e477316abd532be8fdb21facc939addf4f
1eehanjin <12181670@office.inha.ac.kr>
2023-12-10 16:59:49 +0900

    바이낸스 론 가능한 금액 계산해서 빌리도록

--- notice_loan_bot.py
+++ notice_loan_bot.py
@@ -2,6 +2,7 @@ import json
 import upbit_notice_crawler
 import time
 import ccxt
+import math
 
 def generate_timestamp():
     timestamp = int(time.time() * 1000)
@@ -20,32 +21,58 @@ class CcxtBinance():
         })
         self.binance = ccxt.binance()
 
-    def binance_loan_borrow(self, loanCoin, collateralAmount):
+
+    def on_new_coin_listing_detected(self, symbols):
+        for symbol in symbols:
+                self.binance_borrow_all(symbol)
+
+    def binance_borrow_all(self, symbol):
+        self.binance_loan_borrow(symbol)
+        self.binance_cross_margin_borrow(symbol, 1)
+
+    def binance_loan_borrow(self, symbol):
         timestamp = generate_timestamp()
+        collateralAmount = min(self.binance_calculate_colleteral_max_limit(symbol), self.binance_get_account_free_usdt())
         params_loan_borrow = {
-        'loanCoin' : loanCoin,
+        'loanCoin' : symbol,
         'collateralCoin': 'USDT',
         'collateralAmount' : collateralAmount, #USDT 기준이다.
         'timestamp': timestamp,
         }
-        return(self.binance_with_key.sapiPostLoanFlexibleBorrow(params=params_loan_borrow))
+        print("바이낸스 loan" + str(params_loan_borrow))
+        result_message = self.binance_with_key.sapiPostLoanFlexibleBorrow(params=params_loan_borrow)
+        print(result_message)
+        return(result_message)
+    
+    def binance_calculate_colleteral_max_limit(self, symbol):
+        timestamp = generate_timestamp()
+        params_loanable_assets = {
+            'loanCoin': symbol,
+            'timestamp': timestamp
+        }
+        loan_data = self.binance_with_key.sapiGetLoanFlexibleLoanableData(params=params_loanable_assets)
+        loan_max_limit = loan_data['rows'][0]['flexibleMaxLimit']
+        colleteral_max_limit = float(loan_max_limit) * 10 / 7
+        colleteral_max_limit = math.floor(colleteral_max_limit)
+        return colleteral_max_limit
 
-    def binance_cross_margin_borrow(self, asset, amount): 
+
+    def binance_get_account_free_usdt(self):
+        account_balance = self.binance_with_key.fetch_balance()
+        free_usdt_balance =  float(account_balance['USDT']['free'])
+        free_usdt_balance = math.floor(free_usdt_balance)
+        return free_usdt_balance
+
+    def binance_cross_margin_borrow(self, symbol, amount): 
         timestamp = generate_timestamp()
         params_margin_loan = {
-        'asset' : asset,
+        'asset' : symbol,
         'amount': amount, #USDT 기준이 아닌 빌리는 코인 기준으로 수량 입력해야 한다.
         'timestamp': timestamp,
         }
         return(self.binance_with_key.sapiPostMarginLoan(params=params_margin_loan))
     
-    def on_new_coin_listing_detected(self, symbols):
-        for symbol in symbols:
-                ccxtBinance.binance_borrow_all(symbol)
 
-    def binance_borrow_all(self, symbol):
-        self.binance_loan_borrow(symbol, 50)
-        self.binance_cross_margin_borrow(symbol, 1)
 
 
 
--- tests/ccxt_test.py
+++ tests/ccxt_test.py
@@ -2,6 +2,7 @@ import unittest
 import ccxt
 import time
 import json
+import math
 import pprint
 
 def generate_timestamp():
@@ -26,13 +27,48 @@ class CcxtTests(unittest.TestCase):
     def test_print_available_binance_functions(self):
         print(dir(ccxt.binance())) 
 
+    def test_binance_calculate_colleteral_max_limit(self):
+        timestamp = generate_timestamp()
+        params_loanable_assets = {
+            'loanCoin': 'SLP',
+            'timestamp': timestamp
+        }
+        loan_data = self.binance_with_key.sapiGetLoanFlexibleLoanableData(params=params_loanable_assets)
+        loan_max_limit = loan_data['rows'][0]['flexibleMaxLimit']
+        print(loan_max_limit)
+        colleteral_max_limit = float(loan_max_limit) * 10 / 7
+        colleteral_max_limit = math.floor(colleteral_max_limit)
+        print(colleteral_max_limit)
+
     def test_binance_get_flexible_loan_data(self):
         timestamp = generate_timestamp()
         params_loanable_assets = {
-            'loanCoin': 'ETH',
+            'loanCoin': 'SLP',
             'timestamp': timestamp
         }
         print(self.binance_with_key.sapiGetLoanFlexibleLoanableData(params=params_loanable_assets))
+
+    def test_binance_calculate_colleteral_max_limit(self):
+        timestamp = generate_timestamp()
+        params_loanable_assets = {
+            'loanCoin': 'SLP',
+            'timestamp': timestamp
+        }
+        loan_data = self.binance_with_key.sapiGetLoanFlexibleLoanableData(params=params_loanable_assets)
+        loan_max_limit = loan_data['rows'][0]['flexibleMaxLimit']
+        print(loan_max_limit)
+        colleteral_max_limit = float(loan_max_limit) * 10 / 7
+        colleteral_max_limit = math.floor(colleteral_max_limit)
+        print(colleteral_max_limit)
+
+    def test_get_account_balance(self):
+        account_balance = self.binance_with_key.fetch_balance()
+        pprint.pprint(account_balance)
+
+    def test_get_account_free_usdt(self):
+        account_balance = self.binance_with_key.fetch_balance()
+        free_usdt_balance =  account_balance['USDT']['free']
+        pprint.pprint(free_usdt_balance)
 
     def test_binance_loan_borrow(self):
         timestamp = generate_timestamp()
@@ -75,11 +111,13 @@ class CcxtTests(unittest.TestCase):
         }
         print(self.binance_with_key.sapiPostCapitalWithdrawApply(params=params_withdraw))
 
+
+
 # unittest를 실행
 if __name__ == '__main__':  
     # 테스트 스위트 생성
     suite = unittest.TestSuite()
-    suite.addTest(CcxtTests('test_binance_withdraw'))  # 실행할 테스트 추가
+    suite.addTest(CcxtTests('test_get_account_free_usdt'))  # 실행할 테스트 추가
     # suite.addTest(MyTestCase('test_two'))  # 다른 테스트 추가
 
     # 테스트 실행
